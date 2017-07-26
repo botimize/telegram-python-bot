029f2fb1d158ace4aa1cd4be0b80f3a0cbdafed0
Ubuntu <ubuntu@ip-172-31-26-174.ap-northeast-1.compute.internal>
2017-07-26 06:39:34 +0000

    Change to new url (getBotimize.com)

--- echoBot.py
+++ echoBot.py
@@ -10,7 +10,9 @@ dispatcher = updater.dispatcher
 
 # Declare Botimize
 botimize_apiKey = 'Your_Botimize_Api_Key'
-botimize = Botimize(botimize_apiKey, 'generic')
+botimize = Botimize(botimize_apiKey, 'telegram', {
+    'apiUrl': 'https://api.getbotimize.com',
+})
 
 # Customize function
 def start(bot, update):
@@ -18,32 +20,16 @@ def start(bot, update):
 
 def resp(bot, update):
     echo_response = update.message.text
-    bot.sendMessage(chat_id=update.message.chat_id, text=echo_response)
-    
+    bot.sendMessage(chat_id=update.message.chat.id, text=echo_response)
     # botimize incoming
-    incomingLog = {
-        'sender': {
-          'id': update.message.chat_id,
-          'name': 'unkown_user'
-        },
-        'content': {
-            'type': 'text', 
-            'text':  echo_response
-        }
-    }
-    botimize.log_incoming(incomingLog)
+    botimize.log_incoming(update.to_dict())
     # botimize outgoing
     outgoingLog = {
-        'receiver': {
-          'id': update.message.chat_id,
-          'name': 'USER_SCREEN_NAME'
-        },
-        'content': {
-            'type': 'text',
-            'text': echo_response
-        }
+        'token': telegram_bot_token,
+        'chat_id': update.message.chat.id,
+        'text': echo_response
     }
-    print(botimize.log_outgoing(outgoingLog))
+    botimize.log_outgoing(outgoingLog)
 
 # Structuralize bot (custom functions -> dispatcher)
 start_handler = CommandHandler('start', start)
@@ -54,4 +40,3 @@ dispatcher.add_handler(echo_handler)
 # Update to telegram platfrom
 updater.start_polling()
 updater.idle()
-
